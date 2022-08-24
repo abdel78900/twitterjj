d83a838c3b33e8099367d40ac7aa218d7f3f8d62
abdel78900 <vmnagmb@gmail.com>
2022-08-24 19:53:04 +0200

    second commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,12 +10,12 @@ export default NextAuth({
     }),
     // ...add more providers
   ],
-  callbacks:{
-      async session({session, token}){
-          session.user.tag = session.user.name.split(" ").join("").toLocaleLowerCase();
+  // callbacks:{
+  //     async session({session, token}){
+  //         session.user.tag = session.user.name.split(" ").join("").toLocaleLowerCase();
 
-          session.user.uid = token.sub;
-          return session;
-      }
-  }
+  //         session.user.uid = token.sub;
+  //         return session;
+  //     }
+  // }
 })
