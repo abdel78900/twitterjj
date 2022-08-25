656ca3e57990f63437d9f4846b0ee6858d42d116
abdel78900 <vmnagmb@gmail.com>
2022-08-25 06:57:35 +0200

    test11

--- components/Login.js
+++ components/Login.js
@@ -19,7 +19,7 @@ function Login({ providers }) {
               className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group"
               onClick={() => signIn(provider.id, { callbackUrl: "/" })}
             >
-              <span className="w-48 h-48 rounded rotate-[-40deg] bg-[#1d9bf0] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+              <span className="w-64 h-56  rounded rotate-[-40deg] bg-[#1d9bf0] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
               <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">
                 Se connecter avec {provider.name}
               </span>
